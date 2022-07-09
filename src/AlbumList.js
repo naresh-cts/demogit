19b739b5a17058d23c3b30ce4c87113f30e540a1
unknown <cogjava1776@iiht.tech>
2022-07-09 16:48:28 +0530

    myform

---
+++ src/AlbumList.js
@@ -0,0 +1,37 @@
+import{useState} from "react";
+import  './AlbumList.css' ;
+function AlbumList(){ 
+    getAlbums();
+    let[photos, setPhotos] = useState([]);  // in oder to trigger a
+    
+    if(photos.length){
+        getAlbums();
+    }
+    async function getAlbums(){
+        photos = await(await fetch("https://jsonplaceholder.typicode.com/photos")).json(); //why twice
+        console.log(photos);
+        setPhotos(photos);
+
+        }
+   return(
+       <div>
+           <table>  
+               <thead>
+                        <form input></form>
+                 <td> Title</td>
+                   <td>URL</td>
+                   </thead>   
+                   <tbody>                                                             
+           {
+               photos.map( function (photo)  {
+                   // console.log(photo);
+                   return <tr><td>{photo.title}</td>
+                   <td>{photo.url}</td></tr>           
+               
+               })
+           } </tbody>
+           </table>
+       </div>
+               )
+        }
+    export default AlbumList;
